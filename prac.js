01f48723e314dbd18b05554c48be30e11960bf36
Sai Vignesh Golla <100998531+GodsScion@users.noreply.github.com>
2024-01-19 09:26:05 -0800

    bug fixed

--- prac.js
+++ prac.js
@@ -1,7 +1,7 @@
 // const x = 2; let y = 4; function update(arg) { return Math.random() + y * arg; } y = 2; y=3; const result = update(x); console.log(result);;
 
-var longestConsecutive = function() {
-    let nums = []
+
+var longestConsecutive = function(nums) {
     let maxSeqLen = 0
     while (maxSeqLen < nums.length) {
         let num = nums.pop()
@@ -9,12 +9,12 @@ var longestConsecutive = function() {
             num--
             nums.splice(nums.indexOf(num),1)
         }
-        let longest = nums
+        let longest = num
         while (nums.includes(longest+1)) {
             longest++
             nums.splice(nums.indexOf(longest),1)
         }
-        maxSeqLen = Math.max(longest - nums + 1, maxSeqLen)
+        maxSeqLen = Math.max(longest - num + 1, maxSeqLen)
     }
     return maxSeqLen;
 };
