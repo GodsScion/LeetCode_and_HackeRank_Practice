dcdbeaf9faaf9977bdff343f00c1a6dd772714fd
Sai Vignesh Golla <100998531+GodsScion@users.noreply.github.com>
2024-01-19 11:56:36 -0800

    debugged

--- prac.java
+++ prac.java
@@ -1,7 +1,7 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
-    public int longestConsecutive(int[] nums) {
+    public static  int longestConsecutive(int[] nums) {
         if (nums.length == 0) return 0;
         Arrays.sort(nums);
         int prev = nums[0];
@@ -11,11 +11,18 @@ class Solution {
             if (prev + 1 == num) {
                 seq++;
                 maxSeq = Math.max(maxSeq, seq);
-            } else if (prev != num) {
+            } else {
                 seq = 1;
             }
             prev = num;
         }
         return maxSeq;
+    }
+
+    public static void main(String[] args) {  
+        int[][] questions = {{1, 2, 4}, {2, 4, 5}};
+        for (int[] question : questions) {
+            System.out.println(longestConsecutive(question));
+        }
     }
 }
