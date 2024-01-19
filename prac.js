b60c832b8ad7d0b4bc3a3a8c085e2f484c254813
Sai Vignesh Golla <100998531+GodsScion@users.noreply.github.com>
2024-01-19 09:20:52 -0800

    updated

--- prac.js
+++ prac.js
@@ -1,21 +1,20 @@
 // const x = 2; let y = 4; function update(arg) { return Math.random() + y * arg; } y = 2; y=3; const result = update(x); console.log(result);;
 
-var longestConsecutive = function(nums) {
-    let numSet = new Set(nums);
-    let maxSeqLen = 0;
-    while (maxSeqLen < numSet.size) {
-        let num = [...numSet][0];
-        let longest = num + 1;
-        while (numSet.has(longest)) {
-            numSet.delete(longest);
-            longest++;
+var longestConsecutive = function() {
+    let nums = []
+    let maxSeqLen = 0
+    while (maxSeqLen < nums.length) {
+        let num = nums.pop()
+        while (nums.includes(num-1)) {
+            num--
+            nums.splice(nums.indexOf(num),1)
         }
-        num = num - 1;
-        while (numSet.has(num)) {
-            numSet.delete(num);
-            num--;
+        let longest = nums
+        while (nums.includes(longest+1)) {
+            longest++
+            nums.splice(nums.indexOf(longest),1)
         }
-        maxSeqLen = Math.max(maxSeqLen, longest - num - 1);
+        maxSeqLen = Math.max(longest - nums + 1, maxSeqLen)
     }
     return maxSeqLen;
 };
