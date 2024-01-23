af58455c5fb4f4b15aa4676a21de84b193cd60e0
Sai Vignesh Golla <100998531+GodsScion@users.noreply.github.com>
2024-01-23 15:10:01 -0800

    prob 1239

--- prac.py
+++ prac.py
@@ -1,28 +1,12 @@
-def prob(s,k):
+def prob(s):
         maxL = 0
-        l = 0
-        while l < len(s):
-            tempK = k
-            r = l
-            while r < len(s) and s[r] == s[l]:  r+=1
-            newC = r
-            while tempK > 0 and r < len(s) and s[r] != s[l]:
-                r += 1
-                tempK -= 1
-            while r < len(s) and s[r] == s[l]:
-                while r < len(s) and s[r] == s[l]:  r+=1
-                while r < len(s) and s[r] != s[l] and tempK > 0:
-                    r += 1
-                    tempK -= 1
-            maxL = max(maxL, min(r-l+tempK, len(s)) )
-            l = newC
+        
         return maxL
 
 
 questions = [
-    ("ABAB",2),
-    ("AABABBA",1)
+    []
 ]
 
-for s,k in questions:
-    print(prob(s,k))
+for s in questions:
+    print(prob(s))
