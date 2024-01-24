8411779471d58f82f7bd60ee42d00913b0f8985f
Sai Vignesh Golla <100998531+GodsScion@users.noreply.github.com>
2024-01-23 17:05:33 -0800

    prob 219

--- prac.py
+++ prac.py
@@ -1,20 +1,32 @@
 from collections import Counter, defaultdict
 
-def prob(s):
-        maxL = 0
-        chSet = defaultdict(set)
-        properWords = {}
-        goldenWords = {}
-        for i,word in enumerate(s):
-            if len(set(word)) != len(word): continue
-            for c in word:  chSet[c].add(i)
-        
-        return maxL
+def prob(nums,k):
+        i = 0
+        window = set()
+        for i in range(min(k,len(nums))):
+            if nums[i] in window: return True
+            
 
 
-questions = [
-    ["un","iq","ue"]
+testCases = [
+    ([1,2,3,4,5,6,7,7,8],5)      
 ]
 
-for s in questions:
-    print(prob(s))
+for num,k in testCases: print(prob(num,k))
+
+# def prob(s):
+#         maxL = 0
+#         chSet = defaultdict(set)
+#         properWords = {}
+#         goldenWords = {}
+#         for i,word in enumerate(s):
+#             if len(set(word)) != len(word): continue
+#             for ch in word:  chSet[ch].add(i)
+        
+#         return maxL
+# questions = [
+#     ["un","iq","ue"]
+# ]
+
+# for s in questions:
+#     print(prob(s))
