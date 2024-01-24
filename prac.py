d3653184962b72be2916056b75e57d8ab7c02570
Sai Vignesh Golla <100998531+GodsScion@users.noreply.github.com>
2024-01-23 16:49:33 -0800

    prelim

--- prac.py
+++ prac.py
@@ -1,11 +1,19 @@
+from collections import Counter, defaultdict
+
 def prob(s):
         maxL = 0
+        chSet = defaultdict(set)
+        properWords = {}
+        goldenWords = {}
+        for i,word in enumerate(s):
+            if len(set(word)) != len(word): continue
+            for c in word:  chSet[c].add(i)
         
         return maxL
 
 
 questions = [
-    []
+    ["un","iq","ue"]
 ]
 
 for s in questions:
