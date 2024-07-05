b74a1a76d20017f36cb67ce653d55d625099c8fb
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2024-07-04 18:44:45 -0700

    prob 217

--- prac.java
+++ prac.java
@@ -1,37 +1,52 @@
 import java.util.*;
 
+// Question 217
 class Solution {
-    public static int maxProductDifference(int[] nums) {
-        int max1 = 0, max2 = 0, min1 = 100000, min2 = 100000;
-        for(int num: nums){
-            if (num > max1) {max2=max1; max1=num;} else if (num > max2) {max2=num;}
-            if (num < min1) {min2=min1; min1=num;} else if (num < min2) {min2=num;}
-        }
-        return max2*max1 - min1*min2;
-    }
-
-    public static  int longestConsecutive(int[] nums) {
-        if (nums.length == 0) return 0;
-        Arrays.sort(nums);
-        int prev = nums[0];
-        int seq = 1;
-        int maxSeq = 1;
-        for (int num : nums) {
-            if (prev + 1 == num) {
-                seq++;
-                maxSeq = Math.max(maxSeq, seq);
-            }  else if (prev != num) {
-                seq = 1;
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> prevNums = new HashSet<Integer>();
+        for(int num: nums) {
+            if(prevNums.contains(num)) {
+                return true;
             }
-            prev = num;
+            prevNums.add(num);
         }
-        return maxSeq;
-    }
-
-    public static void main(String[] args) {  
-        int[][] questions = {{1, 2, 4,4 ,4,7,8}, {2, 4, 5, 9, 1, 10}, {1,2,0,1, 5, 7, 3}};
-        for (int[] question : questions) {
-            System.out.println(maxProductDifference(question));
-        }
+        return false;
     }
 }
+
+
+// class Solution {
+//     public static int maxProductDifference(int[] nums) {
+//         int max1 = 0, max2 = 0, min1 = 100000, min2 = 100000;
+//         for(int num: nums){
+//             if (num > max1) {max2=max1; max1=num;} else if (num > max2) {max2=num;}
+//             if (num < min1) {min2=min1; min1=num;} else if (num < min2) {min2=num;}
+//         }
+//         return max2*max1 - min1*min2;
+//     }
+
+//     public static  int longestConsecutive(int[] nums) {
+//         if (nums.length == 0) return 0;
+//         Arrays.sort(nums);
+//         int prev = nums[0];
+//         int seq = 1;
+//         int maxSeq = 1;
+//         for (int num : nums) {
+//             if (prev + 1 == num) {
+//                 seq++;
+//                 maxSeq = Math.max(maxSeq, seq);
+//             }  else if (prev != num) {
+//                 seq = 1;
+//             }
+//             prev = num;
+//         }
+//         return maxSeq;
+//     }
+
+//     public static void main(String[] args) {  
+//         int[][] questions = {{1, 2, 4,4 ,4,7,8}, {2, 4, 5, 9, 1, 10}, {1,2,0,1, 5, 7, 3}};
+//         for (int[] question : questions) {
+//             System.out.println(maxProductDifference(question));
+//         }
+//     }
+// }
