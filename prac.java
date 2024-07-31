76b5a6aecf648d6c4f3fd5f0e991eb031bb917b8
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2024-07-30 17:29:46 -0700

    Update prac.java

--- prac.java
+++ prac.java
@@ -460,10 +460,10 @@ class Solution {
                     left = mid+1;
                 }
             } else {
-                if (target >= nums[left] && target < nums[mid]) {
-                    right = mid-1;
-                } else {
+                if (target > nums[mid] && target <= nums[right]) {
                     left = mid+1;
+                } else {
+                    right = mid-1;
                 }
             }
         }
