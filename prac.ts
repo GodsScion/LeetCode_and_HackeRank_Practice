c8e010f2d40ed1771ec34859ce07852bdcd05979
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2024-08-05 14:45:00 -0700

    141

--- prac.ts
+++ prac.ts
@@ -1,1 +1,26 @@
+// ##### LINKED LIST ##### //
+// 141
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
 
+function hasCycle(head: ListNode | null): boolean {
+    let runner: ListNode | null = head;
+    let chaser: ListNode | null = head;
+    while (runner && runner.next) {
+        runner = runner.next.next;
+        chaser = chaser.next;
+        if ( runner === chaser ) {
+            return true;
+        }
+    }
+    return false;
+};
