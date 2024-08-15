8fa114e008a7463c2f333e3dd30eaf360e355419
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2024-08-15 16:51:52 -0700

    Update prac.ts

--- prac.ts
+++ prac.ts
@@ -49,3 +49,23 @@ function invertTree(root: TreeNode | null): TreeNode | null {
     root.right = invertTree(temp);
     return root;
 };
+
+// 104. Maximum Depth of Binary Tree (https://leetcode.com/problems/maximum-depth-of-binary-tree/description/)
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+function maxDepth(root: TreeNode | null): number {
+    if (root == null) return 0;
+    return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+};
+
