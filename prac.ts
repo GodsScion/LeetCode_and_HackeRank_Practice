35e76ea295f0d2e3002895904451f92293cceae5
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2024-08-15 12:12:27 -0700

    Update prac.ts

--- prac.ts
+++ prac.ts
@@ -11,7 +11,6 @@
  *     }
  * }
  */
-
 function hasCycle(head: ListNode | null): boolean {
     let runner: ListNode | null = head;
     let chaser: ListNode | null = head;
@@ -23,4 +22,30 @@ function hasCycle(head: ListNode | null): boolean {
         }
     }
     return false;
+};
+
+
+
+// ##### TREES ##### //
+
+// 226. Invert Binary Tree (https://leetcode.com/problems/invert-binary-tree/description/)
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+function invertTree(root: TreeNode | null): TreeNode | null {
+    if (!root) return null;
+    const temp: TreeNode | null = root.left;
+    root.left = invertTree(root.right);
+    root.right = invertTree(temp);
+    return root;
 };
