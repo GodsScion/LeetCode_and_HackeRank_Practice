e5112d56a05cfa463f5c0f60c416560eb4e9de2f
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2024-10-09 22:08:07 -0400

    128

--- Leet Code/python.py
+++ Leet Code/python.py
@@ -112,6 +112,26 @@ class Solution:
         return True
 
 
+# 128. Longest Consecutive Sequence (https://leetcode.com/problems/longest-consecutive-sequence/description/) - Medium
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        numSet = set(nums)
+        maxSeqLen = 0
+        while maxSeqLen < len(numSet):
+            num = numSet.pop()
+            longest = num+1
+            while longest in numSet:
+                numSet.remove(longest)
+                longest += 1
+            num = num-1
+            while num in numSet:
+                numSet.remove(num)
+                num -= 1
+            maxSeqLen = max(maxSeqLen, longest-num-1)
+        return maxSeqLen
+
+
+
 
 #######  SLIDING WINDOW  #######
 # 76. Minimum Window Substring (https://leetcode.com/problems/minimum-window-substring/description/) - Hard
