730e2d69f40a0a12a27dc0dda228ecd3e4a86b1f
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2025-01-01 19:39:43 -0800

    js scratch pad

--- scratch pad/javascript.js
+++ scratch pad/javascript.js
@@ -3,6 +3,25 @@ function isInt(input) {
     return Number.isInteger(input);
 }
 
+// JS collections
+
+// Set
+let set = new Set();
+
+// Map
+let map = new Map();
+
+
+// Array
+let arr = [1, 2, 3];
+
+// Objects
+let obj = { a: 1, b: 2 };
+
+// Priority Queue
+
+
+
 
 function main() {
     let tests = ["45.7", "45", NaN, null, undefined, {}, [], '4', 20.5, -5, 20];
