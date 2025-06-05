72a3bf51b09a29fd354773a5109c86fbacdac68c
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2025-06-04 22:10:42 -0700

    feat(155): python - min stack

--- Leet Code/python.py
+++ Leet Code/python.py
@@ -282,6 +282,42 @@ class Solution:
         return need == have
 
 
+
+#######  STACK  #######
+# 155. Min Stack (https://leetcode.com/problems/min-stack/description/) - Medium
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+
+class MinStack:
+
+    def __init__(self):
+        self.minStack = []
+        self.stack = []
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        if self.minStack:
+            self.minStack.append(min(self.minStack[-1], val))
+        else:
+            self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop(-1)
+        self.minStack.pop(-1)
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
+
+
+
+
 ####### LINKED LIST #######
 class ListNode:
     def __init__(self, x):
