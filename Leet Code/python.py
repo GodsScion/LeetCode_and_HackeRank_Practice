903db18683ff40c3faf64ffff772cc58c2ecdd26
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2025-06-17 16:13:38 -0700

    feat(100): python

--- Leet Code/python.py
+++ Leet Code/python.py
@@ -363,6 +363,21 @@ class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right)) if root else 0
 
+# 100. Same Tree (https://leetcode.com/problems/same-tree/description/) - Easy
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if p == None and q == None:
+            return True
+        if p == None or q == None or p.val != q.val:
+            return False
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+
 
 
 
