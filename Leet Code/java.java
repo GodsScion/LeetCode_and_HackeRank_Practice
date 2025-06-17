3e6868caf598f41c552dd2552e98f1badfb716ba
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2025-06-17 16:20:32 -0700

    feat(100): java

--- Leet Code/java.java
+++ Leet Code/java.java
@@ -1050,7 +1050,33 @@ class Solution {
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
     }
 
+    // 100. Same Tree (https://leetcode.com/problems/same-tree/description/) - Easy
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode() {}
+     *     TreeNode(int val) { this.val = val; }
+     *     TreeNode(int val, TreeNode left, TreeNode right) {
+     *         this.val = val;
+     *         this.left = left;
+     *         this.right = right;
+     *     }
+     * }
+     */
+    public boolean isSameTree(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        }
+        if (p == null || q == null || p.val != q.val) {
+            return false;
+        }
+        return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+    }
 
+    
     //#######  2-D DYNAMIC PROGRAMMING  #######//
     // 62. Unique Paths (https://leetcode.com/problems/unique-paths/description/) - Medium
     int[][] visited;
