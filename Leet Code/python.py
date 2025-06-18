4cd35bd5c850ded2bca8f3ff324bfcb0bf39ca6d
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2025-06-18 11:35:18 -0700

    feat(572): python optimal

--- Leet Code/python.py
+++ Leet Code/python.py
@@ -404,6 +404,21 @@ class Solution:
         if root1 == None or root2 == None or root1.val != root2.val:
             return False
         return self.isSametree(root1.left, root2.left) and self.isSametree(root1.right, root2.right)
+# 572. Subtree of Another Tree (https://leetcode.com/problems/subtree-of-another-tree/description/) - Easy
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        return self.serialize(subRoot) in self.serialize(root)
+    
+    def serialize(self, node: Optional[TreeNode]) -> str:
+        if node == None: return "N"
+        return f"({node.val},{self.serialize(node.left)},{self.serialize(node.right)})"
+
 
 
 ###### TRIES ######
