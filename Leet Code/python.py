26837601dc0c99e80701affae7ce6efe283d4769
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2025-06-18 10:18:57 -0700

    feat(572): python

--- Leet Code/python.py
+++ Leet Code/python.py
@@ -378,7 +378,27 @@ class Solution:
             return False
         return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
+# 572. Subtree of Another Tree (https://leetcode.com/problems/subtree-of-another-tree/description/) - Easy
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSubtree(self, root: Optional[TreeNode], subRoot: Optional[TreeNode]) -> bool:
+        if self.isSametree(root, subRoot):
+            return True
+        if root != None:
+            return self.isSubtree(root.left, subRoot) or self.isSubtree(root.right, subRoot)
+        return False
 
+    def isSametree(self,  root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        if root1 == None and root2 == None:
+            return True
+        if root1 == None or root2 == None or root1.val != root2.val:
+            return False
+        return self.isSametree(root1.left, root2.left) and self.isSametree(root1.right, root2.right)
 
 
 ###### TRIES ######
