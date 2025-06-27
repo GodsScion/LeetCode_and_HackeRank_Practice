9916b15f7f0b018e1e034659c2189390ac7e5359
Sai Vignesh Golla <saivigneshgolla@outlook.com>
2025-06-26 17:44:07 -0700

    feat(102): python

--- Leet Code/python.py
+++ Leet Code/python.py
@@ -485,6 +485,31 @@ class Solution:
             return None
         self.searchedRight.add(node)
         return self.search(node.right, targets, path)
+
+
+# 102. Binary Tree Level Order Traversal (https://leetcode.com/problems/binary-tree-level-order-traversal/description/) - Medium
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        levels = []
+        queue = [root]
+        while queue:
+            level = []
+            newQueue = []
+            for node in queue:
+                if node != None:
+                    level.append(node.val)
+                    newQueue.append(node.left)
+                    newQueue.append(node.right)
+            if level:
+                levels.append(level)
+            queue = newQueue
+        return levels
             
 
 
